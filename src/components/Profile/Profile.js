baf0535b4761db7fb5a32d1ddbf54a7585abf626
Igor Manukhov <m_igor97@mail.ru>
2022-10-15 00:01:08 +0300

    add profile

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Profile/Profile.css'
+
+function Profile({ onLoginIn }) {
+
+  return (
+    <div className="login">
+
+    </div >
+  )
+}
+
+export default Profile;
