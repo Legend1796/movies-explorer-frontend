c921926a55ea10d7b152a80fd836419e451c0673
Igor Manukhov <m_igor97@mail.ru>
2022-10-21 16:01:22 +0300

    404 link

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 
 function NotFound() {
 
   return (
     <div className='not-found'>
-      <h3 className='not-found__title'>404</h3>
-      <p className='not-found__text'>Страница не найдена</p>
-      <Link className='button-to-main' to='/main'>Назад</Link>
+      <Route path="*">
+        <h3 className='not-found__title'>404</h3>
+        <p className='not-found__text'>Страница не найдена</p>
+        <Link className='button-to-main' to='/main'>Назад</Link>
+      </Route>
     </div>
   )
 }
