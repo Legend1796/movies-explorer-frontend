e55d5a00c2bbb016a23629729c43f9929b0727f8
Igor Manukhov <m_igor97@mail.ru>
2022-10-21 23:10:23 +0300

    767w navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,37 +8,37 @@ function Navigation({ onClose, isOpen, profileImage, closeNavigationBtn, exitPro
   }
 
   return (
-    <div className={`popup ${isOpen ? 'popup_opened' : ''}`}>
-      <div className='popup__block' >
-        <img src={closeNavigationBtn} onClick={onClose} className="popup__close" type="button" alt="Закрыть" />
-        <div className='popup__navigation'>
+    <div className={`navigation ${isOpen ? 'navigation_opened' : ''}`}>
+      <div className='navigation__block' >
+        <img src={closeNavigationBtn} onClick={onClose} className="navigation__close" type="button" alt="Закрыть" />
+        <div className='navigation__container'>
           <Route path="/movies">
-            <div className='popup__link-container'>
-              <Link onClick={loggedOut} className='popup__link' to='/main'><p className='popup__link-text'>Главная</p></Link>
-              <Link onClick={onClose} className='popup__link' to='/movies'><div className='popup__link-text'>Фильмы<div className='popup__underline-movies popup__underline-movies_active' /></div></Link>
-              <Link onClick={onClose} className='popup__link' to='/saved-movies'><p className='popup__link-text'>Сохранённые фильмы<div className='popup__underline-movies' /></p></Link>
+            <div className='navigation__links'>
+              <Link onClick={loggedOut} className='navigation__link' to='/main'><p className='navigation__link-text'>Главная</p></Link>
+              <Link onClick={onClose} className='navigation__link' to='/movies'><div className='navigation__link-text navigation__link-text_active'>Фильмы</div></Link>
+              <Link onClick={onClose} className='navigation__link' to='/saved-movies'><p className='navigation__link-text'>Сохранённые фильмы<div className='navigation__underline-movies' /></p></Link>
             </div>
-            <Link onClick={onClose} className='popup__link popup__link-profile' to='/profile'><p className='popup__link-text'>Аккаунт</p><img className='popup__profile-image' src={profileImage} alt='значок профиля' /></Link>
+            <Link onClick={onClose} className='navigation__link navigation__link-profile' to='/profile'><p className='navigation__link-text'>Аккаунт</p><img className='navigation__profile-image' src={profileImage} alt='значок профиля' /></Link>
           </Route>
           <Route path="/saved-movies">
-            <div className='popup__link-container'>
-              <Link onClick={loggedOut} className='popup__link' to='/main'><p className='popup__link-text'>Главная</p></Link>
-              <Link onClick={onClose} className='popup__link' to='/movies'><p className='popup__link-text'>Фильмы<div className='popup__underline-movies' /></p></Link>
-              <Link onClick={onClose} className='popup__link' to='/saved-movies'><div className='popup__link-text'>Сохранённые фильмы<div className='popup__underline-movies popup__underline-movies_active' /></div></Link>
+            <div className='navigation__link-container'>
+              <Link onClick={loggedOut} className='navigation__link' to='/main'><p className='navigation__link-text'>Главная</p></Link>
+              <Link onClick={onClose} className='navigation__link' to='/movies'><p className='navigation__link-text'>Фильмы<div className='navigation__underline-movies' /></p></Link>
+              <Link onClick={onClose} className='navigation__link' to='/saved-movies'><div className='navigation__link-text'>Сохранённые фильмы<div className='navigation__underline-movies navigation__underline-movies_active' /></div></Link>
             </div>
-            <Link onClick={onClose} className='popup__link popup__link-profile' to='/profile'><p className='popup__link-text'>Аккаунт</p><img className='popup__profile-image' src={profileImage} alt='значок профиля' /></Link>
+            <Link onClick={onClose} className='navigation__link navigation__link-profile' to='/profile'><p className='navigation__link-text'>Аккаунт</p><img className='navigation__profile-image' src={profileImage} alt='значок профиля' /></Link>
           </Route>
           <Route path="/profile">
-            <div className='popup__link-container'>
-              <Link onClick={loggedOut} className='popup__link' to='/main'><p className='popup__link-text'>Главная</p></Link>
-              <Link onClick={onClose} className='popup__link' to='/movies'><p className='popup__link-text'>Фильмы<div className='popup__underline-movies' /></p></Link>
-              <Link onClick={onClose} className='popup__link' to='/saved-movies'><p className='popup__link-text'>Сохранённые фильмы<div className='popup__underline-movies' /></p></Link>
+            <div className='navigation__link-container'>
+              <Link onClick={loggedOut} className='navigation__link' to='/main'><p className='navigation__link-text'>Главная</p></Link>
+              <Link onClick={onClose} className='navigation__link' to='/movies'><p className='navigation__link-text'>Фильмы<div className='navigation__underline-movies' /></p></Link>
+              <Link onClick={onClose} className='navigation__link' to='/saved-movies'><p className='navigation__link-text'>Сохранённые фильмы<div className='navigation__underline-movies' /></p></Link>
             </div>
-            <Link onClick={onClose} className='popup__link popup__link-profile' to='/profile'><p className='popup__link-text'>Аккаунт</p><img className='popup__profile-image' src={profileImage} alt='значок профиля' /></Link>
+            <Link onClick={onClose} className='navigation__link navigation__link-profile' to='/profile'><p className='navigation__link-text'>Аккаунт</p><img className='navigation__profile-image' src={profileImage} alt='значок профиля' /></Link>
           </Route>
         </div>
       </div >
-      <button onClick={onClose} className="popup__overlay" />
+      <button onClick={onClose} className="navigation__overlay" />
     </div>
   )
 }
