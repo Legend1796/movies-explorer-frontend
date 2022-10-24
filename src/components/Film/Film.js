967d1a11be35376ebb82ce04bb4d4101818ca427
Igor Manukhov <m_igor97@mail.ru>
2022-10-24 22:30:30 +0300

    film rref

--- src/components/Film/Film.js
+++ src/components/Film/Film.js
@@ -25,18 +25,17 @@ function Film({ filmInfo, onFilmSave, onFilmDelete }) {
   return (
     <li className='element'>
       <div className='element__container'>
-        <img className='element__image' src={filmInfo.link} alt='Фото места' />
         <div className='element__rectangle'>
           <h2 className='element__title'>{filmInfo.name}</h2>
-          <Route path='/movies'>
-            <img className='element__save' src={isSaved ? saveOn : saveOff} onClick={handlefilmSave} type='button' aria-label='В избранное' />
-          </Route>
-          <Route path='/saved-movies'>
-            <img className='element__save' src={deletefilm} onClick={handlefilmDelete} type='button' aria-label='В избранное' />
-          </Route>
+          <p className='element__time'>{filmInfo.duration}</p>
         </div>
-        <div className='element__underline' />
-        <p className='element__time'>{filmInfo.duration}</p>
+        <img className='element__image' src={filmInfo.link} alt='Фото места' />
+        <Route path='/movies'>
+          <img className='element__save' src={isSaved ? saveOn : saveOff} onClick={handlefilmSave} type='button' aria-label='В избранное' />
+        </Route>
+        <Route path='/saved-movies'>
+          <img className='element__save' src={deletefilm} onClick={handlefilmDelete} type='button' aria-label='В избранное' />
+        </Route>
       </div>
     </li>
   )
