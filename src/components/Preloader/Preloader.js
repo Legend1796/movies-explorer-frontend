858ce3547b0e0a11f6842376212d31329bcfe66d
Igor Manukhov <m_igor97@mail.ru>
2022-10-30 22:22:22 +0300

    preloader work

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -3,7 +3,7 @@ import '../Preloader/Preloader.css'
 
 function Preloader({ isLoading }) {
     return (
-        <div className={`preloader ${!isLoading ? 'preloader_opened' : ''}`}>
+        <div className={`preloader ${isLoading ? 'preloader_opened' : ''}`}>
             <div className="preloader__container">
                 <span className="preloader__round"></span>
             </div>
