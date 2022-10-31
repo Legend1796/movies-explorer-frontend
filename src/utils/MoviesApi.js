b3553de04876f679d3247eb00bd5b88ba69b0d1f
Igor Manukhov <m_igor97@mail.ru>
2022-10-31 11:18:28 +0300

    create MoviesApi

---
+++ src/utils/MoviesApi.js
@@ -0,0 +1,34 @@
+export class MoviesApi {
+  constructor(options) {
+    this._url = options.baseUrl;
+    this._headers = options.headers;
+  }
+
+  // getUserInfo() {
+  //   return fetch(`${this._url}/users/me`, {
+  //     method: 'GET',
+  //     credentials: "include",
+  //     headers: this._headers
+  //   })
+  //     .then((res) => {
+  //       return this._getResponseData(res);
+  //     });
+  // }
+
+
+  _getResponseData(res) {
+    if (res.ok) {
+      return res.clone().json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  }
+}
+
+const moviesApi = new MoviesApi({
+  baseUrl: 'https://api.nomoreparties.co/beatfilm-movies ',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+export default moviesApi;
